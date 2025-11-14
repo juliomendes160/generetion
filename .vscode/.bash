3719cdaed7019e608aabe773a8434985922dc106
Julio Mendes <juliomendes160@hotmail.com>
2025-11-14 17:37:49 -0300

    final commit

---
+++ .vscode/.bash
@@ -0,0 +1,16 @@
+# "$(cygpath -p "\Program Files\Microsoft VS Code\bin\code")" -v
+
+# "$(cygpath -p "\Program Files\Git\cmd\git.exe")" -v
+
+# "$(cygpath -p "\Program Files\node\node.exe")" -v
+
+# cmd "/c rmdir "E:\\Program Files\\Microsoft VS Code""
+# cmd "/c mklink /D "E:\\Program Files\\Microsoft VS Code" "\\code\\1.105.1\\VSCode-win32-x64-1.105.1""
+
+# cmd "/c rmdir "E:\\Program Files\\Git""
+# cmd "/c mklink /D "E:\\Program Files\\Git" "\\git\\2.51.2\\PortableGit""
+
+# cmd "/c rmdir "E:\\Program Files\\node""
+# cmd "/c mklink /D "E:\\Program Files\\node" "\\node\\24.11.1\\node-v24.11.1-win-x64\\node-v24.11.1-win-x64""
+
+# cmd '/c robocopy D:\ F:\ DEV.vhdx /Z /MT:16'
