800488226e79f32948afe0b5c48b007bd8ae585d
Julio Mendes <juliomendes160@hotmail.com>
2026-01-29 12:03:18 -0300

    chore: configurar banco de dados

--- blogpessoal/src/app.module.ts
+++ blogpessoal/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: "mysql",
+			host: "localhost",
+			port: 3306,
+			username: "root",
+			password: "root",
+			database: "db_blogpessoal",
+			entities: [],
+			synchronize: true,
+			logging: true,
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
