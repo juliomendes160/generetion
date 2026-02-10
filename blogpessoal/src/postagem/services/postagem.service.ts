b2da43e2dfeffbe8519e1eb8afc1500a63bca27d
Julio Mendes <juliomendes160@hotmail.com>
2026-02-09 23:29:30 -0300

    9. Projeto Blog Pessoal - Módulo Postagem Controller e Service - Método Buscar por título

--- blogpessoal/src/postagem/services/postagem.service.ts
+++ blogpessoal/src/postagem/services/postagem.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { ILike, Repository } from 'typeorm';
 import { Postagem } from '../entities/postagem.entity';
 
 @Injectable()
@@ -25,5 +25,13 @@ export class PostagemService {
       throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
 
     return postagem;
+  }
+
+  async findAllByTitulo(titulo: string): Promise<Postagem[]> {
+    return await this.postagemRepository.find({
+      where: {
+        titulo: ILike(`%${titulo}%`),
+      },
+    });
   }
 }
