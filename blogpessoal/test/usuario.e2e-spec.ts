44a81d275a6d6647b628e28e90e6a0ed29058b3d
Julio Mendes <juliomendes160@hotmail.com>
2026-02-13 19:39:56 -0300

    22. Projeto Blog Pessoal - Teste de Software - Configurando o ambiente de testes

---
+++ blogpessoal/test/usuario.e2e-spec.ts
@@ -0,0 +1,32 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('Testes dos Módulos Usuário e Auth (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          entities: [__dirname + './../src/**/entities/*.entity.ts'],
+          synchronize: true,
+          dropSchema: true,
+        }),
+        AppModule,
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
