8adfefe2c478e31c3eafcd4b8275c423769a4c63
Julio Mendes <juliomendes160@hotmail.com>
2026-02-19 14:04:08 -0300

    19. Projeto Blog Pessoal - Página de Login - Lógica

--- blogpessoal/src/components/navbar/Navbar.tsx
+++ blogpessoal/src/components/navbar/Navbar.tsx
@@ -1,6 +1,18 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const { handleLogout } = useContext(AuthContext);
+
+  function logout() {
+    handleLogout();
+    alert("O Usuário foi desconectado com sucesso!");
+    navigate("/");
+  }
+
   return (
     <>
       <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
@@ -9,7 +21,10 @@ function Navbar() {
             Blog Pessoal
           </Link>
           <div className="flex gap-4">
-            Postagens Temas Cadastrar tema Perfil Sair
+            Postagens Temas Cadastrar tema Perfil{" "}
+            <Link to="" onClick={logout} className="hover:underline">
+              Sair
+            </Link>
           </div>
         </div>
       </div>
