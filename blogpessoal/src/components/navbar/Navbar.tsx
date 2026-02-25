47b1e4748ad0e0995c553ff52380b620aeecc6a5
Julio Mendes <juliomendes160@hotmail.com>
2026-02-24 23:25:18 -0300

    33. Projeto Blog Pessoal - Ajustes Finais - Parte 01

--- blogpessoal/src/components/navbar/Navbar.tsx
+++ blogpessoal/src/components/navbar/Navbar.tsx
@@ -1,11 +1,11 @@
-import { useContext } from "react";
+import { useContext, type ReactNode } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
   const navigate = useNavigate();
 
-  const { handleLogout } = useContext(AuthContext);
+  const { usuario, handleLogout } = useContext(AuthContext);
 
   function logout() {
     handleLogout();
@@ -13,13 +13,16 @@ function Navbar() {
     navigate("/");
   }
 
-  return (
-    <>
+  let component: ReactNode;
+
+  if (usuario.token !== "") {
+    component = (
       <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
         <div className="container flex justify-between text-lg mx-8">
           <Link to="/home" className="text-2xl font-bold">
             Blog Pessoal
           </Link>
+
           <div className="flex gap-4">
             <Link to="/postagens" className="hover:underline">
               Postagens
@@ -39,8 +42,10 @@ function Navbar() {
           </div>
         </div>
       </div>
-    </>
-  );
+    );
+  }
+
+  return <>{component}</>;
 }
 
 export default Navbar;
